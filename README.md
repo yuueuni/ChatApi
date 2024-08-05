07ec56a2917c70595c2fd961eb302fbae5b91070
yuueuni <yuueuni@gmail.com>
2024-08-05 19:01:02 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chat API
+- This is a simple chat API that uses OpenAI's GPT-4o-Mini to generate responses to user messages.
+
+## Project
+- Python 3.12.x
+- FastAPI 0.112.0
+- openai 1.38.0
+
---
+++ openai_test.py
@@ -0,0 +1,19 @@
+
+from openai import OpenAI
+import os
+
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
